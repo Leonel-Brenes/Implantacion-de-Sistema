d7b8f28ddbc7da2428e7bcd122dcb8ca78719714
Leonel-Brenes <131913971+Leonel-Brenes@users.noreply.github.com>
2024-11-16 22:44:08 -0600

    Delete IMPSPROJECT-master directory

--- IMPSPROJECT-master/src/public/js/manejoAlertas.js
+++
@@ -1,8 +0,0 @@
-// Desaparecer la alerta después de 5 segundos (5000 ms)
-setTimeout(function () {
-  document.querySelector(".alert-success").style.display = "none";
-}, 3000);
-// Desaparecer la alerta después de 5 segundos (5000 ms)
-setTimeout(function () {
-  document.querySelector(".alert-danger").style.display = "none";
-}, 3000);
